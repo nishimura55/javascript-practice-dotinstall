1379851b969b5e4a6315995a31fa040ecb70f226
nishimura55 <naoki.hnmr55@gmail.com>
2020-06-13 20:45:13 +0900

    ストップワォッチ

---
+++ MyStopWatch/js/main.js
@@ -0,0 +1,69 @@
+'use strict';
+
+{
+    const timer = document.getElementById('timer');
+    const start = document.getElementById('start');
+    const stop = document.getElementById('stop');
+    const reset = document.getElementById('reset');
+
+    let startTime;
+    let timeoutId;
+    let elapsedTime = 0;
+
+    function countUp() {
+        const d = new Date(Date.now() - startTime + elapsedTime);
+        const m = String(d.getMinutes()).padStart(2, '0');
+        const s = String(d.getSeconds()).padStart(2,'0');
+        const ms = String(d.getMilliseconds()).padStart(3, '0');
+        timer.textContent = `${m}:${s}.${ms}`;
+
+        timeoutId = setTimeout(() => {
+            countUp();
+        }, 10);
+    }
+
+    function setButtonStateInitial () {
+        start.classList.remove('inactive')
+        stop.classList.add('inactive')
+        reset.classList.add('inactive')
+    }
+    function setButtonStateRuning () {
+        start.classList.add('inactive')
+        stop.classList.remove('inactive')
+        reset.classList.add('inactive')
+    }
+    function setButtonStateState () {
+        start.classList.remove('inactive')
+        stop.classList.add('inactive')
+        reset.classList.remove('inactive')
+    }
+
+    setButtonStateInitial();
+
+    start.addEventListener('click', () => {
+        if (start.classList.contains('inactive') === true) {
+            return;
+        }
+        setButtonStateRuning();
+        startTime = Date.now();
+        countUp();
+    });
+
+    stop.addEventListener('click', () => {
+        if (stop.classList.contains('inactive') === true) {
+            return;
+        }
+        setButtonStateState();
+        clearTimeout(timeoutId);
+        elapsedTime += Date.now() - startTime;
+    });
+
+    reset.addEventListener('click', () => {
+        if (reset.classList.contains('inactive') === true) {
+            return;
+        }
+       setButtonStateInitial();
+       timer.textContent = '00:00.000';
+       elapsedTime = 0;
+    });
+}
