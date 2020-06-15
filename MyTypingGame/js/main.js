6ea9a44ae821a4d3f107b734e14c19059ddb16e4
nishimura55 <naoki.hnmr55@gmail.com>
2020-06-15 21:30:03 +0900

    タイピングゲーム2

--- MyTypingGame/js/main.js
+++ MyTypingGame/js/main.js
@@ -1,7 +1,36 @@
 'use strict';
 
 {
+    const word = 'apple';
+    let loc = 0;
+    let score = 0;
+    let miss = 0;
+
+    const target = document.getElementById('target');
+    const scoreLabel = document.getElementById('score');
+    const missLabel = document.getElementById('miss');
+
+    target.textContent = word;
+
+    function updateTarget() {
+        let placeholder = '';
+        for (let i = 0; i < loc; i++) {
+            placeholder += '_';
+        }
+        target.textContent = placeholder + word.substring(loc);
+    }
+
     window.addEventListener('keydown', (e) => {
-        console.log(e.key);
+        if (e.key === word[loc]) {
+            console.log('score');
+            loc++;
+            updateTarget();
+            score++;
+            scoreLabel.textContent = score;
+        } else {
+            console.log('miss');
+            miss++;
+            missLabel.textContent = miss;
+        }
     });
 }
